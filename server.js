ef7299b6e9801f63231c6a984d43b96c589b5bc9
unknown <feriel.benkraiem@ensi-uma.tn>
2025-01-13 16:08:36 +0100

    back

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.post('/accept-order/:id', async (req, res) => {
         }
 
         const workflowDispatchUrl = `https://api.github.com/repos/comweave/Pipelines_Version2/actions/workflows/github-workflow.yml/dispatches`;
-        const GITHUB_TOKEN = "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"; 
+        const GITHUB_TOKEN = "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"; 
 
         const workflowInputs = {
             versioningTool: order.versioningTool,
